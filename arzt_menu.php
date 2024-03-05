d8757db7feb3937fdee70984c38c712d8d196c3e
rese02 <147379536+rese02@users.noreply.github.com>
2024-03-05 20:46:53 +0100

    Delete arzt_menu.php

--- arzt_menu.php
+++
@@ -1,4 +0,0 @@
-<a href="db_patienten_liste.php">Alle Patienten zeigen</a> &nbsp;&nbsp;&nbsp; ||&nbsp;&nbsp;&nbsp;
-<a href="db_patient_neu.php">Neue Patienten erfassen</a> &nbsp;&nbsp;&nbsp; ||&nbsp;&nbsp;&nbsp;
-<a href="db_medikamente_liste.php">Alle Medikamente zeigen</a> &nbsp;&nbsp;&nbsp; ||&nbsp;&nbsp;&nbsp;
-<a href="db_medikament_neu.php">Neue Medikamente erfassen</a>
